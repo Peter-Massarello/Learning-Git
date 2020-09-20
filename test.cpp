8c04994e8170dc4087d8bb7220058b46776f5f88
Peter Massarello <pmassarello@gmail.com>
2020-09-19 19:31:00 -0700

    test again

---
+++ test.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout << "Hello world";
+    int a = 5;
+    for (int i = 0; i < a; i++){
+        cout << "test";
+    }
+    return 0;
+}
